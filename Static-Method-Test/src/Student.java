94a615733422c4b242d98a87f36258ac9445eb3c
SonHustle <132913431+xuanson1@users.noreply.github.com>
2024-06-14 19:41:34 +0700

    Add files via upload

--- Static-Method-Test/src/Student.java
+++ Static-Method-Test/src/Student.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private int rollno;
+    private String name;
+    private static String college = "BBDIT";
+
+    Student(int r, String n) {
+        rollno = r;
+        name = n;
+    }
+
+    static void change(){
+        college = "CODEGYM";
+    }
+
+    void display(){
+        System.out.println(rollno + " " + name + " " + college);
+    }
 }
