6c91b01207a3f0df6de1f35a8648a0188af45615
xuanson1 <hustle50k@gmail.com>
2024-06-21 19:59:11 +0700

    Example

---
+++ LinkedList_Stack/src/GenericStackClient.java
@@ -0,0 +1,8 @@
+public class GenericStackClient {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        MyGenericStack.stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+        MyGenericStack.stackOfIStrings();
+    }
+}
